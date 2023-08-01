f8768dfc29f2cc05079857662fc21d07281b953b
selenyel <selenyelyazilim@gmail.com>
2023-08-01 17:17:52 +0300

    Last Skeletal Touches has been finished
    The form works as intended purpose. I want to add an eye icon to show and hide the password for my next step.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,27 @@ export class AppComponent {
 
   @ViewChild('myForm') theForm : NgForm;
 
- 
+  subscriptions = ["Basic", "Advanced", "Pro"]
+  hide = true;
+  submitted = false;
+
   formDatas = {
     email : '',
-    
+    subscriptionType : this.subscriptions[1],
+    password:'',
   }
  
 
-  onSubmit(){}
+  onSubmit(){
+    console.log('my form', this.theForm)
+    this.formDatas.email = this.theForm.value.email;
+    this.formDatas.subscriptionType = this.theForm.value.subscription == undefined ? 'Advanced' : this.theForm.value.subscription;
+    this.formDatas.password = this.theForm.value.password;
+    console.log('my email', this.formDatas.email,
+      'my subscription type', this.formDatas.subscriptionType,
+      'my password', this.formDatas.password
+    );
+
+    this.submitted = true;
+  }
 }
